bc92fe738f95b0a6bef32d5e6f83a58f7209ca63
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2020-01-21 18:54:08 +0100

    [entitybuilder] use id class
    Moreover, this commit removes the bool in the function arguments to
    addSubDomain() to indicate whether or not a sub-domain is to be taken as
    confining or not. Now, there are two interfaces:
    
    * addSubDomain(...) -> adds a non-confining sub-domain
    * addConfiningSubDomain(...) -> adds a confining sub-domain

--- test/io/test_write_gmsh.cc
+++ test/io/test_write_gmsh.cc
@@ -13,10 +13,13 @@
 
 #include <frackit/geometry/cylinder.hh>
 #include <frackit/geometry/disk.hh>
-#include <frackit/precision/precision.hh>
+
 #include <frackit/magnitude/magnitude.hh>
 #include <frackit/magnitude/containedmagnitude.hh>
+
+#include <frackit/common/id.hh>
 #include <frackit/occ/breputilities.hh>
+#include <frackit/precision/precision.hh>
 
 #include <frackit/sampling/pointsampling.hh>
 #include <frackit/sampling/geometrysampling.hh>
@@ -28,9 +31,10 @@
 //! create 3 disk-shaped fractures and write geo (gmsh file format) file
 int main(int argc, char** argv)
 {
+    using namespace Frackit;
     using ctype = double;
-    using Domain = Frackit::Cylinder<ctype>;
-    using Disk = Frackit::Disk<ctype>;
+    using Domain = Cylinder<ctype>;
+    using Disk = Disk<ctype>;
     using Point = typename Disk::Point;
     using Direction = typename Disk::Direction;
     using Vector = typename Direction::Vector;
@@ -43,18 +47,18 @@ int main(int argc, char** argv)
     Domain domain(0.5, 1.0);
     Domain domain2(Disk(Point(0.0, 0.0, 1.0), e1, e2, 1.0, 1.0), 1.0);
 
-    Frackit::ContainedEntityNetworkBuilder builder;
-    builder.addSubDomain(domain, 1);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.1), e1, e2, 1.0, 1.0), 1);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.5), e1, e3, 2.0, 2.0), 1);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.75), e1, e2, 1.0, 1.0), 1);
+    ContainedEntityNetworkBuilder builder;
+    builder.addSubDomain(domain, Id(1));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.1), e1, e2, 1.0, 1.0),  Id(1));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.5), e1, e3, 2.0, 2.0),  Id(1));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 0.75), e1, e2, 1.0, 1.0), Id(1));
 
-    builder.addSubDomain(domain2, 2);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.1), e1, e2, 1.0, 1.0),2);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.5), e1, e3, 2.0, 2.0),2);
-    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.75), e1, e2, 1.0, 1.0),2);
+    builder.addSubDomain(domain2, Id(2));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.1), e1, e2, 1.0, 1.0),  Id(2));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.5), e1, e3, 2.0, 2.0),  Id(2));
+    builder.addSubDomainEntity(Disk(Point(0.0, 0.0, 1.75), e1, e2, 1.0, 1.0), Id(2));
 
-    Frackit::GmshWriter writer(builder.build());
+    GmshWriter writer(builder.build());
     writer.write("final", 1.0, 1.0);
 
     std::cout << "All tests passed" << std::endl;
