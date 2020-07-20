4f266d18ff8b8dee06309ccf52342656660de745
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2020-07-20 13:22:11 +0200

    [python][occ][__init__] do not overwrite getShape()

--- python/frackit/occutilities/__init__.py
+++ python/frackit/occutilities/__init__.py
@@ -5,7 +5,7 @@ from ._occutilities import *
 ###########################################################
 
 # helper function to get the wrapped shape of an object
-def getShape(object):
+def getShapeWrapper(object):
     try: return OCCShapeWrapper(_occutilities.getShape(object))
     except: raise TypeError("Given object cannot be wrapped into an occ shape")
 
@@ -19,7 +19,7 @@ def cut(object, tool, eps):
     tool: The shape to be cut out of the object.
     eps: Tolerance value to be used.
     """
-    return _occutilities.cut(getShape(object), getShape(tool), eps)
+    return _occutilities.cut(getShapeWrapper(object), getShapeWrapper(tool), eps)
 
 # intersection operator
 def intersect(object, tool, eps):
@@ -31,7 +31,7 @@ def intersect(object, tool, eps):
     tool: The tool shape.
     eps: Tolerance value to be used.
     """
-    return _occutilities.intersect(getShape(object), getShape(tool), eps)
+    return _occutilities.intersect(getShapeWrapper(object), getShapeWrapper(tool), eps)
 
 # fragmentation operator
 def fragment(shapeList, eps):
@@ -42,7 +42,7 @@ def fragment(shapeList, eps):
     shapeList: List of shapes whose fragments are to be computed.
     eps: Tolerance value to be used.
     """
-    return _occutilities.fragment([getShape(shape) for shape in shapeList], eps)
+    return _occutilities.fragment([getShapeWrapper(shape) for shape in shapeList], eps)
 
 # fusion operator
 def fuse(shapeList, eps):
@@ -53,4 +53,4 @@ def fuse(shapeList, eps):
     shapeList: List of shapes whose fusion is to be computed.
     eps: Tolerance value to be used.
     """
-    return _occutilities.fuse([getShape(shape) for shape in shapeList], eps)
+    return _occutilities.fuse([getShapeWrapper(shape) for shape in shapeList], eps)
