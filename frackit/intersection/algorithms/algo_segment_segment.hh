41f4d2dcce7dbbaeb885ba1ababc0115bf238f97
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2020-11-04 12:54:34 +0100

    [intersect][seg-seg] fix algo for touching points

--- frackit/intersection/algorithms/algo_segment_segment.hh
+++ frackit/intersection/algorithms/algo_segment_segment.hh
@@ -110,8 +110,7 @@ intersect_segment_segment(const Segment<ctype, wd>& segment1,
             for (TopExp_Explorer explorer(edge, TopAbs_VERTEX); explorer.More(); explorer.Next())
             {
                 auto curPoint = OCCUtilities::point(TopoDS::Vertex(explorer.Current()));
-                if (!curPoint.isEqual(s1, eps) && !curPoint.isEqual(t1, eps)
-                    && !curPoint.isEqual(s2, eps) && !curPoint.isEqual(t2, eps))
+                if (segment1.contains(curPoint, eps) && segment2.contains(curPoint, eps))
                     return ResultType( std::move(curPoint) );
             }
         }
